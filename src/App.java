6f7e598a98896c1d7e4b5ef880f0b6d5cc563da2
Juan Manuel <gonzcarjuan06@gmail.com>
2025-04-29 10:20:43 +0100

    feat(App.java): display menu options

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        System.out.println("Menu");
+        System.out.println("- opcion 1: añadir persona.");
+        System.out.println("- opcion 2: borrar persona.");
+        System.out.println("- opcion 3: salir.");
     }
 }
